281ac34c7c3a894258ef4e439b256ed3e1df965b
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-03-02 19:42:51 -0500

    :sparkles: rename documents_db

--- src/chatbot/dynamodb.tf
+++ src/chatbot/dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "my_dynamodb_table" {
+resource "aws_dynamodb_table" "documents_db" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = var.dynamodb_hash_key
