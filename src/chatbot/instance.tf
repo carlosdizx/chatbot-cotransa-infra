bcf02ffe9fb2d1f11eed2f0fc55c9f324301b441
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-03-02 19:13:18 -0500

    :sparkles: used script in user data

--- src/chatbot/instance.tf
+++ src/chatbot/instance.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "chatbot" {
   key_name               = aws_key_pair.deployer_key.key_name
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+  user_data                   = file("scripts/python_script.sh")
+  user_data_replace_on_change = true
   tags = {
     Name = "Chatbot Instance"
   }
