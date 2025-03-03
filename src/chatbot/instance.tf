af0bec11e8cdb37795d0536ed6a9e66da67c87ab
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-03-02 19:12:09 -0500

    :sparkles: used config network

--- src/chatbot/instance.tf
+++ src/chatbot/instance.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "chatbot" {
-  ami           = var.details_instances.ami
-  instance_type = var.details_instances.type
-  key_name      = aws_key_pair.deployer_key.key_name
+  ami                    = var.details_instances.ami
+  instance_type          = var.details_instances.type
+  key_name               = aws_key_pair.deployer_key.key_name
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
   tags = {
     Name = "Chatbot Instance"
   }
