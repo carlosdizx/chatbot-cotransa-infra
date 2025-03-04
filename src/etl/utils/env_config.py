93b614f9298f30702ef423df5274257dec50df2d
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-03-03 21:46:16 -0500

    :sparkles: create env config

---
+++ src/etl/utils/env_config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+
+def load_config() -> dict:
+    load_dotenv()
+
+    return {
+        "PDF_LIST_FILE": os.getenv("PDF_LIST_FILE", "data/URLsS3.txt"),
+        "S3_BUCKET_NAME": os.getenv("S3_BUCKET_NAME"),
+    }
