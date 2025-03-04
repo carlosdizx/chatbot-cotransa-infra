d16ee48af66c0db49c51d71378a42d614fe408b0
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-03-03 22:09:19 -0500

    :sparkles: create public bucket in s3 and generate random name

---
+++ src/etl/s3.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "cotransa-bucket-${random_uuid.uuid.result}"
+}
+
+resource "aws_s3_bucket_ownership_controls" "public_access" {
+  bucket = aws_s3_bucket.public_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket                  = aws_s3_bucket.public_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+  ignore_public_acls      = false
+}
+
+resource "aws_s3_bucket_policy" "public_access_policy" {
+  bucket = aws_s3_bucket.public_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.public_bucket.id}/*"
+      }
+    ]
+  })
+}
